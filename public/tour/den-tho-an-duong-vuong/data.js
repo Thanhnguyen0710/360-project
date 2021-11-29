63a72fbfd4d271299bc4c7dd410ce42c97920850
Thanhnguyen0710 <56418634+Thanhnguyen0710@users.noreply.github.com>
2021-11-29 20:13:00 +0700

    add 360

---
+++ public/tour/den-tho-an-duong-vuong/data.js
@@ -0,0 +1,427 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-khu-di-tch-c-loa",
+      "name": "Khu Di Tích Cổ Loa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.011285077142146,
+        "pitch": -0.07917044253487937,
+        "fov": 1.3274289077235746
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.132143417699723,
+          "pitch": -0.018859206414678198,
+          "rotation": 0,
+          "target": "1-cng-n-th-an-dng-vng"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cng-n-th-an-dng-vng",
+      "name": "Cổng Đền Thờ An Dương Vương",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.4873131438517788,
+        "pitch": -0.10697513739471276,
+        "fov": 1.3274289077235746
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.007913685808224713,
+          "pitch": -0.10055949731925651,
+          "rotation": 6.283185307179586,
+          "target": "2-tam-quan"
+        },
+        {
+          "yaw": -1.321089208860334,
+          "pitch": -0.011086309546763928,
+          "rotation": 0,
+          "target": "0-khu-di-tch-c-loa"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7597933289097814,
+          "pitch": -0.00009813960002702515,
+          "title": "Giếng Ngọc",
+          "text": "",
+          "popup": "tiles/pop-up/Gieng-Ngoc.png"
+        }
+      ]
+    },
+    {
+      "id": "2-tam-quan",
+      "name": "Tam Quan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.1549429150639785,
+        "pitch": -0.03498494781828221,
+        "fov": 1.3274289077235746
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7802440174336072,
+          "pitch": -0.04281871506202073,
+          "rotation": 7.853981633974483,
+          "target": "1-cng-n-th-an-dng-vng"
+        },
+        {
+          "yaw": -1.101026843895463,
+          "pitch": 0.04343103820287375,
+          "rotation": 5.497787143782138,
+          "target": "3-miu-th-thn-nng"
+        },
+        {
+          "yaw": -0.28959989623651694,
+          "pitch": -0.14341130055619544,
+          "rotation": 0,
+          "target": "5-nh-tin-ng"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.955045017381,
+          "pitch": 0.12988610674681667,
+          "title": "Giếng Cổ",
+          "text": "",
+          "popup": "tiles/pop-up/Gieng.png"
+        }
+      ]
+    },
+    {
+      "id": "3-miu-th-thn-nng",
+      "name": "Miếu Thờ Thần Nông",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8626417172389136,
+          "pitch": 0.2725978348158513,
+          "rotation": 7.853981633974483,
+          "target": "2-tam-quan"
+        },
+        {
+          "yaw": -0.015887551373864994,
+          "pitch": 0.013693190974059632,
+          "rotation": 0,
+          "target": "4-nh-bia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-nh-bia",
+      "name": "Nhà Bia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5788879332981729,
+          "pitch": 0.00713738966135935,
+          "rotation": 4.71238898038469,
+          "target": "3-miu-th-thn-nng"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6741316330489866,
+          "pitch": -0.060366303565210444,
+          "title": "Bia Cổ",
+          "text": "Đây là nơi lưu giữ các bia đá cổ, trong đó có ba bia được khắc năm 1606.",
+          "popup": "tiles/pop-up/Nha-Bia.png"
+        }
+      ]
+    },
+    {
+      "id": "5-nh-tin-ng",
+      "name": "Nhà Tiền Đường",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.128847221060389,
+          "pitch": 0.6289844731620118,
+          "rotation": 0,
+          "target": "2-tam-quan"
+        },
+        {
+          "yaw": 0.08277449291295547,
+          "pitch": 0.35962605023858707,
+          "rotation": 0,
+          "target": "6-thng-in"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-thng-in",
+      "name": "Thượng Điện",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.1123736742974337,
+        "pitch": -0.007496774532487649,
+        "fov": 1.3274289077235746
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11708910135661377,
+          "pitch": 0.03782165151120864,
+          "rotation": 0,
+          "target": "5-nh-tin-ng"
+        },
+        {
+          "yaw": -3.047171877011655,
+          "pitch": -0.11825618156688833,
+          "rotation": 0,
+          "target": "8-ca-thng-in"
+        },
+        {
+          "yaw": -2.279226385215969,
+          "pitch": 0.004026212618105873,
+          "rotation": 0,
+          "target": "7-dy-phi-thng-in"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-dy-phi-thng-in",
+      "name": "Dãy Phải Thượng Điện",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.003458593966929,
+          "pitch": 0.15565758611909075,
+          "rotation": 10.995574287564278,
+          "target": "6-thng-in"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-ca-thng-in",
+      "name": "Cửa Thượng Điện",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07720995917971507,
+          "pitch": 0.05751707161045516,
+          "rotation": 0,
+          "target": "6-thng-in"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0618814570938353,
+          "pitch": -0.2750954896183444,
+          "title": "Bên Trong Thượng Điện",
+          "text": "Text",
+          "popup": "tiles/pop-up/Ban-Tho.png"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
